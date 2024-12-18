bdffbc3bb26a6f8101403b3e7277f741d3e1684b
MaryAmartey <maryamartey07@gmail.com>
2024-12-18 18:36:01 -0500

    run prettier

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -224,9 +224,9 @@ function Navigation(props) {
                     : 'text-white rounded md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700'
                 }`}
               >
-                                  Projects
+                Projects
               </a>
-  </li>
+            </li>
             <li>
               <a
                 href="/content#testimonials"
